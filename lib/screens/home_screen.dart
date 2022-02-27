f77f725b64448561d8d5dd214b6ffb55afa790c7
Abdelmenam-Tarek-Abdelmenam <moneam.elbahy@gmail.com>
2022-02-27 19:01:24 +0200

    add time picker

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,44 +13,83 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  CalendarFormat _calendarFormat = CalendarFormat.twoWeeks;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<AppCubit, AppStates>(
       listener: (BuildContext context, AppStates state) {},
       builder: (BuildContext context, AppStates state) {
-        AppCubit cubit = AppCubit.get(context);
+        // AppCubit cubit = AppCubit.get(context);
         return Scaffold(
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              cubit.dataBase.child("test").update({"val": "val"});
-            },
-            child: const Icon(Icons.eight_k),
-          ),
           appBar: AppBar(
             title: const Text("Calender Control"),
           ),
-          body: TableCalendar(
-              firstDay: DateTime.utc(2010, 10, 16),
-              lastDay: DateTime.utc(2030, 3, 14),
-              calendarFormat: _calendarFormat,
-              onFormatChanged: (format) {
-                setState(() {
-                  _calendarFormat = format;
-                });
-              },
-              focusedDay: _focusedDay,
-              onDaySelected: (selectedDay, focusedDay) {
-                print(selectedDay);
-                setState(() {
-                  _focusedDay = selectedDay;
-                });
-                print(focusedDay);
-              }),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                TableCalendar(
+                  firstDay: DateTime.utc(2010, 10, 16),
+                  lastDay: DateTime.utc(2030, 3, 14),
+                  focusedDay: _focusedDay,
+                  startingDayOfWeek: StartingDayOfWeek.saturday,
+                  calendarFormat: _calendarFormat,
+                  calendarStyle: const CalendarStyle(
+                    outsideDaysVisible: false,
+                  ),
+                  eventLoader: (DateTime date) {
+                    if (date.day % 5 == 0) {
+                      return ["kk", "kk"];
+                    } else {
+                      return [];
+                    }
+                  },
+                  selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                  onDaySelected: (selectedDay, focusedDay) {
+                    if (!isSameDay(_selectedDay, selectedDay)) {
+                      setState(() {
+                        _selectedDay = selectedDay;
+                        _focusedDay = focusedDay;
+                      });
+                    }
+                  },
+                  onFormatChanged: (format) {
+                    if (_calendarFormat != format) {
+                      setState(() {
+                        _calendarFormat = format;
+                      });
+                    }
+                  },
+                  onPageChanged: (focusedDay) {
+                    _focusedDay = focusedDay;
+                  },
+                ),
+                OutlinedButton(
+                    onPressed: () {
+                      _selectTime(context);
+                    },
+                    child: const Text("chooee Start Date")),
+                OutlinedButton(
+                    onPressed: () {
+                      _selectTime(context);
+                    },
+                    child: const Text("choose end Date"))
+              ],
+            ),
+          ),
         );
       },
     );
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    TimeOfDay initialTime = TimeOfDay.now();
+    TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: initialTime,
+    );
+    print(pickedTime);
   }
 }
