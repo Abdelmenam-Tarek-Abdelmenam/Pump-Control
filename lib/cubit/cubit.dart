f73b50f72349f2ba9a1bb4efb0031faa61643032
Abdelmenam-Tarek-Abdelmenam <moneam.elbahy@gmail.com>
2022-02-27 18:25:28 +0200

    add firebase

--- lib/cubit/cubit.dart
+++ lib/cubit/cubit.dart
@@ -1,9 +1,12 @@
 import 'package:calender_app/cubit/states.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppCubit extends Cubit<AppStates> {
   AppCubit() : super(AppInitial());
   static AppCubit get(context) => BlocProvider.of(context);
+
+  final dataBase = FirebaseDatabase.instance.reference();
 
   void setState() {
     emit(GeneralState());
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:table_calendar/table_calendar.dart';
+
+import '../cubit/cubit.dart';
+import '../cubit/states.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,27 +18,39 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Calender Control"),
-      ),
-      body: TableCalendar(
-          firstDay: DateTime.utc(2010, 10, 16),
-          lastDay: DateTime.utc(2030, 3, 14),
-          calendarFormat: _calendarFormat,
-          onFormatChanged: (format) {
-            setState(() {
-              _calendarFormat = format;
-            });
-          },
-          focusedDay: _focusedDay,
-          onDaySelected: (selectedDay, focusedDay) {
-            print(selectedDay);
-            setState(() {
-              _focusedDay = selectedDay;
-            });
-            print(focusedDay);
-          }),
+    return BlocConsumer<AppCubit, AppStates>(
+      listener: (BuildContext context, AppStates state) {},
+      builder: (BuildContext context, AppStates state) {
+        AppCubit cubit = AppCubit.get(context);
+        return Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              cubit.dataBase.child("test").update({"val": "val"});
+            },
+            child: const Icon(Icons.eight_k),
+          ),
+          appBar: AppBar(
+            title: const Text("Calender Control"),
+          ),
+          body: TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              calendarFormat: _calendarFormat,
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              focusedDay: _focusedDay,
+              onDaySelected: (selectedDay, focusedDay) {
+                print(selectedDay);
+                setState(() {
+                  _focusedDay = selectedDay;
+                });
+                print(focusedDay);
+              }),
+        );
+      },
     );
   }
 }
